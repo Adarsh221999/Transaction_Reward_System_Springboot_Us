b382c21f1bf20f9ccae4c3bacf413e8f0ce6d63f
Adarrsh2299 <adarshchimgaonakr123@gmail.com>
2025-06-03 13:40:12 +0530

    Commiting the initial files for the Setup .

---
+++ src/test/java/com/Tranzaction_Reward_System_Springboot_Us/Tranzaction_Reward_System_Springboot_Us/TranzactionRewardSystemSpringbootUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tranzaction_Reward_System_Springboot_Us.Tranzaction_Reward_System_Springboot_Us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranzactionRewardSystemSpringbootUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
