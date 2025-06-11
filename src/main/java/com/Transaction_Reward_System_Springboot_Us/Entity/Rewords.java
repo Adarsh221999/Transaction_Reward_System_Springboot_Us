3ff011f21e677832c8856305cdffc770be8635ca
Adarrsh2299 <adarshchimgaonakr123@gmail.com>
2025-06-11 16:10:53 +0530

    Corrected Spelling Mistakes.

--- src/main/java/com/Transaction_Reward_System_Springboot_Us/Entity/Rewords.java
+++ src/main/java/com/Transaction_Reward_System_Springboot_Us/Entity/Rewords.java
@@ -3,10 +3,14 @@ package com.Transaction_Reward_System_Springboot_Us.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Setter
+@Getter
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Table(name="Rewords" )
@@ -14,7 +18,7 @@ public class Rewords {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer tranzationId;
+    private Integer transationId;
 
     @Column
     private LocalDate date;
@@ -26,7 +30,7 @@ public class Rewords {
 
     @Column
     @NotNull
-    private Double tranzationAmount;
+    private Double transationAmount;
 
     @Column
     private Long rewordPoints;
@@ -34,63 +38,15 @@ public class Rewords {
     @Column(nullable = false , unique = false)
     private Long customerId;
 
-    public Integer getTranzationId() {
-        return tranzationId;
-    }
-
-    public void setTranzationId(Integer tranzationId) {
-        this.tranzationId = tranzationId;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public Double getTranzationAmount() {
-        return tranzationAmount;
-    }
-
-    public void setTranzationAmount(Double tranzationAmount) {
-        this.tranzationAmount = tranzationAmount;
-    }
-
-    public Long getRewordPoints() {
-        return rewordPoints;
-    }
-
-    public void setRewordPoints(Long rewordPoints) {
-        this.rewordPoints = rewordPoints;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
     public Rewords() {}
 
     @Override
     public String toString() {
         return "Rewords{" +
-                "tranzationId=" + tranzationId +
+                "tranzationId=" + transationId +
                 ", date=" + date +
                 ", customerName='" + customerName + '\'' +
-                ", tranzationAmount=" + tranzationAmount +
+                ", transationAmount=" + transationAmount +
                 ", rewordPoints=" + rewordPoints +
                 ", customerId=" + customerId +
                 '}';
@@ -100,11 +56,11 @@ public class Rewords {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Rewords rewords = (Rewords) o;
-        return Objects.equals(tranzationId, rewords.tranzationId) && Objects.equals(date, rewords.date) && Objects.equals(customerName, rewords.customerName) && Objects.equals(tranzationAmount, rewords.tranzationAmount) && Objects.equals(rewordPoints, rewords.rewordPoints) && Objects.equals(customerId, rewords.customerId);
+        return Objects.equals(transationId, rewords.transationId) && Objects.equals(date, rewords.date) && Objects.equals(customerName, rewords.customerName) && Objects.equals(transationAmount, rewords.transationAmount) && Objects.equals(rewordPoints, rewords.rewordPoints) && Objects.equals(customerId, rewords.customerId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(tranzationId, date, customerName, tranzationAmount, rewordPoints, customerId);
+        return Objects.hash(transationId, date, customerName, transationAmount, rewordPoints, customerId);
     }
 }
