cabfe243dc9dc8394456c79495e6ae980161e429
Adarrsh2299 <adarshchimgaonakr123@gmail.com>
2025-06-18 17:16:39 +0530

    Removed Extra Space and Unused Imports.

--- src/main/java/com/Transaction_Reward_System_Springboot_Us/Models/RewardSummeryByCustomer.java
+++ src/main/java/com/Transaction_Reward_System_Springboot_Us/Models/RewardSummeryByCustomer.java
@@ -1,14 +1,10 @@
 package com.Transaction_Reward_System_Springboot_Us.Models;
 
-import com.Transaction_Reward_System_Springboot_Us.Entity.Rewards;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 @Getter
 @Setter
